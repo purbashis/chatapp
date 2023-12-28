d54f0dd6f042d8202932869346c325df7406ff39
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-28 10:11:30 +0530

    adding buttons and modes to the Authentication Screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,6 +8,7 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _islogin = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,39 +19,55 @@ class _AuthScreenState extends State<AuthScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: EdgeInsets.only(
-                  top: 30,bottom: 20,left: 20,right: 20
-                ),
+                margin: const EdgeInsets.only(
+                    top: 30, bottom: 20, left: 20, right: 20),
                 width: 200,
                 child: Image.asset('assets/images/chat.png'),
               ),
               Card(
-                margin: EdgeInsets.all(20),
+                margin: const EdgeInsets.all(20),
                 child: SingleChildScrollView(
-                  child: 
-                  Padding(padding: const EdgeInsets.all(16),
-                  child: Form(child: 
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Email Address'
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        autocorrect: false,
-                        textCapitalization: TextCapitalization.none,
-
-                      ),
-                           TextFormField(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                        child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
                           decoration:
-                              InputDecoration(labelText: 'Password'),
-                         obscureText: true,
-                         
+                              const InputDecoration(labelText: 'Email Address'),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
                         ),
-                    ],
-                  )
-                  ),),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer),
+                            child: Text(_islogin ? 'Login' : 'SignUp')),
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                // _islogin = _islogin ? false : true;
+                                _islogin = !_islogin;
+                              });
+                            },
+                            child: Text(_islogin
+                                ? 'Create an account'
+                                : 'I already have an account '))
+                      ],
+                    )),
+                  ),
                 ),
               ),
             ],
