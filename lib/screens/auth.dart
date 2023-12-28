7b942d31aed8cc71d86cbf1a799db4763810fde5
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-28 09:53:02 +0530

    added Auth screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -13,7 +13,49 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,bottom: 20,left: 20,right: 20
+                ),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: 
+                  Padding(padding: const EdgeInsets.all(16),
+                  child: Form(child: 
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email Address'
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.none,
+
+                      ),
+                           TextFormField(
+                          decoration:
+                              InputDecoration(labelText: 'Password'),
+                         obscureText: true,
+                         
+                        ),
+                    ],
+                  )
+                  ),),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
