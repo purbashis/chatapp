ecb0d8e7af10cb4dfd11197ec56491c12599487c
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-28 15:41:36 +0530

    adding a image picker widget

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius:  40 ,
+          backgroundColor: Colors.grey,
+          foregroundColor: ...,
+        ),
+        TextButton.icon(onPressed: (){}, icon: Icon(Icons.image), label: Text('Add Image',style: TextStyle(
+          color: Theme.of(context).primaryColor
+        ) ,))
+      ],
+    );
+  }
+}
