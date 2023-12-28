3c3602d283990633c0a49aff07a69126d1066a31
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-28 16:06:21 +0530

    using image picker

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/widgets/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -70,6 +71,9 @@ class _AuthScreenState extends State<AuthScreen> {
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: [
+
+                            if(!_isLogin)
+                            UserImagePicker(),
                             TextFormField(
                               decoration: const InputDecoration(
                                   labelText: 'Email Address'),
