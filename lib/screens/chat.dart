0ccd3204e55ca50d30732470c40b73e5fbcbd8cc
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-29 14:24:05 +0530

    sending and reading data to and from firestore

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,6 @@
+import 'package:chatapp/widgets/chat_messages.dart';
+import 'package:chatapp/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -8,22 +9,26 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Chat'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ))
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in !'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter Chat'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.primary,
+                ))
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage()
+          ],
+        ));
   }
 }
